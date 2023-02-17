49eaf18758d135e6f4480a64b1342b3ac8f230df
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-02-17 07:08:49 -0800

    Cleanup and add unit tests.

--- run_split.gd
+++ run_split.gd
@@ -4,7 +4,6 @@ extends EditorScript
 const sperlger_const = preload("res://addons/splerger/split_splerger.gd")
 
 
-# Called when the node enters the scene tree for the first time.
 func _run():
 	var root: Node = get_editor_interface().get_edited_scene_root()
 	sperlger_const.traverse_root_and_split(root)
